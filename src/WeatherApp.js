71eb3659428f8061b78337a334dedd2971f6082a
Aryan <rahulkathait1998@gmail.com>
2024-09-04 15:16:22 +0530

    commit

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import './App.css';
 function WeatherApp() {
   const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
@@ -30,40 +30,53 @@ function WeatherApp() {
 
   return (
     <div className="container">
-      <input
-        type="text"
-        className="form-control mb-3"
-        placeholder="Enter city name"
-        aria-label="City name"
-        aria-describedby="basic-addon2"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-      />
-      <button 
-        className="btn btn-outline-secondary search-button mb-3" 
-        type="button" 
-        onClick={fetchWeather}
-      >
-        Search
-      </button>
-      {loading && <p>Loading data...</p>}
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter city name"
+          aria-label="City name"
+          aria-describedby="basic-addon2"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <div className="input-group-append">
+          <button 
+            className="btn btn-outline-secondary search-button" 
+            type="button" 
+            onClick={fetchWeather}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      {loading && <p>Loading data…</p>}
       <div className="weather-cards">
         {weatherData && (
           <div className="weather-card">
-            <p className="label">Temperature</p>
-            <p>{weatherData.current.temp_c}°C</p>
-            <p className="label">Humidity</p>
-            <p>{weatherData.current.humidity}%</p>
-            <p className="label">Condition</p>
-            <p>{weatherData.current.condition.text}</p>
-            <p className="label">Wind Speed</p>
-            <p>{weatherData.current.wind_kph}kph</p>
+            <div className="weather-details">
+              <div>
+                <p className="label">Temperature</p>
+                <p>{weatherData.current.temp_c}°C</p>
+              </div>
+              <div>
+                <p className="label">Humidity</p>
+                <p>{weatherData.current.humidity}%</p>
+              </div>
+              <div>
+                <p className="label">Condition</p>
+                <p>{weatherData.current.condition.text}</p>
+              </div>
+              <div>
+                <p className="label">Wind Speed</p>
+                <p>{weatherData.current.wind_kph}kph</p>
+              </div>
+            </div>
           </div>
         )}
       </div>
     </div>
   );
-  
   
   
 }
